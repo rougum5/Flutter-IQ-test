097b799969ab996d2fd30850360102b6726ae2b0
rougum5 <rougum5@gmail.com>
2025-05-19 19:21:21 +0100

    lib

---
+++ main.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IQ Test App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("IQ Test")),
+      body: const Center(child: Text("Welcome to the IQ Test App!")),
+    );
+  }
+}
